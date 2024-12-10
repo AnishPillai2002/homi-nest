ccdc599b375966e571065557459c069a18d4f8ad
Anish Pillai <anishp8914@gmail.com>
2024-12-10 12:09:13 +0530

    initial commit

---
+++ homi-nest-backend/src/main/java/homi_nest/homi_nest/HomiNestApplication.java
@@ -0,0 +1,13 @@
+package homi_nest.homi_nest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomiNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomiNestApplication.class, args);
+	}
+
+}
---
+++ homi-nest-backend/src/test/java/homi_nest/homi_nest/HomiNestApplicationTests.java
@@ -0,0 +1,13 @@
+package homi_nest.homi_nest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomiNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
